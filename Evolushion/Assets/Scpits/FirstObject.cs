60bd83481a596f22c8b04edcc6c8830fee36d2d6
aDovhopolov <dn070291dag@gmail.com>
2019-08-19 13:58:12 +0300

    fade between

---
+++ Evolushion/Assets/Scpits/FirstObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Gamekit2D;
+using UnityEngine;
+
+public class FirstObject : MonoBehaviour
+{
+    void Start()
+    {
+        //ScreenFader.SetAlpha(1f);
+        StartCoroutine(start());
+    }
+
+    IEnumerator start()
+    {
+        yield return new WaitForSeconds(1f);
+        //StartCoroutine (ScreenFader.FadeSceneIn ());
+        
+        yield return StartCoroutine(ScreenFader.FadeSceneOut(ScreenFader.FadeType.Loading));
+        
+        
+        yield return StartCoroutine(ScreenFader.FadeSceneIn());
+    }
+}
